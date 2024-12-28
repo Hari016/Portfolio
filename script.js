6c08eb7375fa875fc1f904b67c5e218bb87c6846
Hari Thagunna <harithagunna8848@gmail.com>
2024-12-28 07:43:49 +0545

    updated
    update

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function hamburg(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(0px)"
-}
+// function hamburg(){
+//     const navbar = document.querySelector(".dropdown")
+//     navbar.style.transform = "translateY(0px)"
+// }
 
-function cancel(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(-500px)"
-}
+// function cancel(){
+//     const navbar = document.querySelector(".dropdown")
+//     navbar.style.transform = "translateY(-500px)"
+// }
 
 // Typewriter Effect
 
@@ -15,23 +15,8 @@ const texts = [
     "DESIGNER",
     "STUDENT"
 ]
-
-let speed  =150;
-const textElements = document.querySelector(".typewriter-text");
-
 let textIndex = 0;
 let charcterIndex = 0;
-
-function typeWriter(){
-    if (charcterIndex < texts[textIndex].length){
-        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
-        charcterIndex++;
-        setTimeout(typeWriter, speed); 
-    }
-    else{
-        setTimeout(eraseText, 1000)
-    }
-}
 
 function eraseText(){
     if(textElements.innerHTML.length > 0){
@@ -44,5 +29,16 @@ function eraseText(){
         setTimeout(typeWriter, 500)
     }
 }
-
+let speed  =150;
+const textElements = document.querySelector(".typewriter-text");
+function typeWriter(){
+    if (charcterIndex < texts[textIndex].length){
+        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
+        charcterIndex++;
+        setTimeout(typeWriter, speed); 
+    }
+    else{
+        setTimeout(eraseText, 1000)
+    }
+}
 window.onload = typeWriter
